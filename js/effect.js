5decf2488236b5e1cea5f6d66a720fa9c7e5fdbd
Deepak Maurya <itsdpm@gmail.com>
2017-09-28 14:36:22 +0530

    Final touch

--- js/effect.js
+++ js/effect.js
@@ -1,9 +1,7 @@
 var trades = ['Programmers', 'Web Designers', 'Developers'];
 var i = 0;
 
-window.onload = function(){
-	setInterval(function(){
-		document.getElementById('trade').innerHTML = trades[i%3];
-		++i;
-	}, 2000);
-}
+setInterval(function(){
+	document.getElementById('trade').innerHTML = trades[i%3];
+	++i;
+}, 2000);
